d1bf2e6be77531b2a25ec198b083aa3d3c6e2a44
Malekai <>
2023-03-24 07:03:38 +0000

    Simplify implementation & remove duplicate checks

---
+++ src/check-request-queries.js
@@ -0,0 +1,44 @@
+import { has_own_property } from "./lib.js";
+
+// Check request query parameters against defined (allowed) query parameters
+export default function(allowed, allowed_entries, request_queries) {
+  // Check to see if any required query parameters are missing
+  for (const [query, definition] of allowed_entries) {
+    if (definition.required === true && !has_own_property(request_queries, query)) {
+      return { error: "missing", target: query };
+    }
+  }
+
+  // Return no error and no target if there are no query parameters to check and no required query parameters specified
+  if (Object.keys(request_queries).length < 1) {
+    return { error: undefined, target: undefined };
+  }
+
+  // Iterate the user provided request queries
+  for (let [query, value] of Object.entries(request_queries)) {
+    // Check to see if the current query has a definition
+    if (!has_own_property(allowed, query)) {
+      return { error: "unknown", target: query };
+    }
+
+    // Get the query definition
+    const definition = allowed[query];
+
+    // Ensure the query parameter value is a string
+    if (typeof request_queries[query] !== "string") {
+      return { error: "type", target: query };
+    }
+
+    // Check to see if the value matches the provided value/values or passes the test against a provided regular expression
+    if (
+      (has_own_property(definition, "value") && definition.value !== value) ||
+      (has_own_property(definition, "values") && !definition.values.includes(value)) ||
+      (has_own_property(definition, "matches") && !definition.matches.test(value))
+    ) {
+      return { error: "value", target: query };
+    }
+  }
+
+  // Return no error and no target if the checks passed
+  return { error: undefined, target: undefined };
+}
